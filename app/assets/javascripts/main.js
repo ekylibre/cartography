4a930797bbe25a4567379cd731df506e6097565c
Brice TEXIER <brice@ekylibre.com>
2017-11-16 13:13:25 +0100

    Disable draw-merge

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,11 +4,14 @@
 //= require leaflet-draw/dist/leaflet.draw-src.js
 //= require leaflet-providers/leaflet-providers.js
 //= require rtree/dist/rtree.js
+
 //= require leaflet-layerindex/leaflet.layerindex.js
 //= require leaflet-snap/leaflet.snap.js
 //= require leaflet-geometryutil/src/leaflet.geometryutil.js
 //= require leaflet-reactive_measure/dist/reactive_measure.js
+
 //= require leaflet-draw-cut/dist/leaflet.draw.cut.js
-//= require leaflet-draw-merge/dist/leaflet.draw.merge.js
+// require leaflet-draw-merge/dist/leaflet.draw.merge.js
 //= require leaflet-controlpanel/dist/leaflet.controlpanel.js
+
 //= require_tree .
